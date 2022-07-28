288f980b54bf21c6fe720da7b12b95e6f8c984f0
balajimahraaj123 <balajimahraaj123@gmail.com>
2022-07-28 20:00:02 +0530

    Init

---
+++ app.js
@@ -0,0 +1,112 @@
+var express    = require('express');
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+var path       = require('path');
+var XLSX       = require('xlsx');
+var multer     = require('multer');
+
+
+//multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  });
+  
+  var upload = multer({ storage: storage });
+
+//connect to db
+mongoose.connect('mongodb://localhost:27017/Demoexcel',{useNewUrlParser:true})
+.then(()=>{console.log('connected to db')})
+.catch((error)=>{console.log('error',error)});
+
+//init app
+var app = express();
+app.use(express.json())
+
+//set the template engine
+app.set('view engine','ejs');
+
+//fetch data from the request
+app.use(bodyParser.urlencoded({extended:false}));
+
+//static folder path
+app.use(express.static(path.resolve(__dirname,'public')));
+
+//collection schema
+var excelSchema = new mongoose.Schema({
+    NAME:String,
+    TYPE:String,
+    BREED:String,
+    AGE:Number
+});
+
+var excelModel = mongoose.model('excelData',excelSchema);
+
+
+app.get('/',(req,res)=>{
+   excelModel.find((err,data)=>{
+       if(err){
+           console.log(err)
+       }else{
+           if(data!=''){
+               res.render('home',{result:data});
+           }else{
+               res.render('home',{result:{}});
+           }
+       }
+   });
+});
+
+app.post('/',upload.single('excel'),(req,res)=>{
+  var workbook =  XLSX.readFile(req.file.path);
+  var sheet_namelist = workbook.SheetNames;
+  var x=0;
+  sheet_namelist.forEach(element => {
+      var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_namelist[x]]);
+      excelModel.insertMany(xlData,(err,data)=>{
+          if(err){
+              console.log(err);
+          }else{
+              console.log(data);
+          }
+      })
+      x++;
+  });
+  res.redirect('/');
+});
+
+app.get('/api/pet',async(req,res)=>{
+    const result= await excelModel.find();
+    res.send(result)
+})
+app.get('/api/pet/:id',async(req,res)=>{
+    const resul= await excelModel.findById({_id:req.params.id});
+    res.send(resul)
+})
+
+app.put('/api/pet/:id',async(req,res)=>{
+    var resul= await excelModel.findById(req.params.id);
+    console.log(resul);
+    if(!resul){
+        return res.send("not finded")
+    }
+    resul=await excelModel.findByIdAndUpdate({_id:req.params.id},req.body)
+    res.send(resul)
+})
+
+app.delete('/api/pet/:id',async(req,res)=>{
+    var resul= await excelModel.findById(req.params.id);
+    if(!resul){
+        return res.send("not finded")
+    }
+    resul=await excelModel.findByIdAndDelete({_id:req.params.id},req.body)
+    res.send(resul)
+})
+
+//assign port
+var port = process.env.PORT || 3000;
+app.listen(port,()=>console.log('server run at '+port));
